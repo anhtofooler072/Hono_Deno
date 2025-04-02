bec1c2c08ee1f5fe2b4930b3aff1c2f0784c249f
QuangAnhTo <quangato072@gmail.com>
2025-04-02 14:14:05 +0700

    feat: update schema for the database

--- database/drizzle/schemas.ts
+++ database/drizzle/schemas.ts
@@ -1,10 +1,12 @@
+import { relations } from "drizzle-orm";
 import {
   pgEnum,
   pgTable,
+  PgTable,
   text,
   timestamp,
   uuid,
-} from "npm:drizzle-orm/pg-core";
+} from "drizzle-orm/pg-core";
 
 export const usersRoles = pgEnum("usersRoles", ["ADMIN", "BASIC"]);
 
@@ -18,12 +20,42 @@ export const usersTable = pgTable("users", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
+export const usersPostsTable = pgTable("users_posts", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  autthorId: uuid("author_id")
+    .notNull()
+    .references(() => usersTable.id),
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
 export const usersCommentsTable = pgTable("users_comments", {
   id: uuid("id").primaryKey().defaultRandom(),
-  userId: uuid("user_id").notNull().references(() => usersTable.id),
+  authorId: uuid("author_id")
+    .notNull()
+    .references(() => usersTable.id),
   comment: text("comment").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
+
+export const postsRelations = relations(usersPostsTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [usersPostsTable.autthorId],
+    references: [usersTable.id],
+  }),
+}));
+
+export const commentsRelations = relations(usersCommentsTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [usersCommentsTable.authorId],
+    references: [usersTable.id],
+  }),
+}));
+
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  comments: many(usersCommentsTable),
+  posts: many(usersPostsTable),
+}));
 
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
